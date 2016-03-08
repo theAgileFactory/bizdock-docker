b0ac1b3a37ff4a4fec7d0d6316bbfb5e042a83ac
Marc Schaer <marc110191@gmail.com>
2016-03-08 10:22:52 +0100

    add possibility to merge properties into an existing Bizdock image

---
+++ deployment-bizdock-image/bizdock/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "---- MERGING PROPERTIES ----"
+/opt/scripts/merge_properties.sh
+
+"echo "---- LAUNCHING BIZDOCK ----"
+/opt/maf/maf-desktop/server/maf-desktop-app-dist/bin/maf-desktop-app -Dcom.agifac.appid=maf-desktop-docker -Dconfig.file    =/opt/maf/maf-desktop/conf/application.conf -Dlogger.file=/opt/maf/maf-desktop/conf/application-logger.xml -Dhttp.port=8000 -DapplyE    volutions.default=false
---
+++ deployment-bizdock-image/install_maven.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /opt/
+wget http://mirror.switch.ch/mirror/apache/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.zip
+unzip apache-maven-3.3.3-bin.zip
+mv apache-maven-3.3.3 maven
