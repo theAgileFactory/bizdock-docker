c3118ee311e52ec1623cfe3868fd8a1722877319
Marc Schaer <marc110191@gmail.com>
2016-03-18 15:28:55 +0100

    update version number in build_images.sh (deploy)

--- deployment-bizdock-image/build_images.sh
+++ deployment-bizdock-image/build_images.sh
@@ -4,4 +4,4 @@ echo "---- BUILDING BIZDOCK DATABASE IMAGE ----"
 docker build -f ./bizdockdb/Dockerfile -t taf/bizdock_mariadb:10.1.12 .
 
 echo "---- BUILDING BIZDOCK IMAGE ----"
-docker build -f ./bizdock/Dockerfile -t taf/bizdock:11.0.1 .
+docker build -f ./bizdock/Dockerfile -t taf/bizdock:12.0.1 .
