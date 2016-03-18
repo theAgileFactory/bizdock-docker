fe3d1b6fd834ffdceef175158e85d1ac04eca5e2
Marc Schaer <marc110191@gmail.com>
2016-03-18 08:51:37 +0100

    add test data to development image (dev)

--- development-bizdock-image/db.sh
+++ development-bizdock-image/db.sh
@@ -4,32 +4,37 @@
 #If "reset" is selected, the database schema will be droped before being re-created
 
 HELP="Possible arguments :
-	--help (-h)
-	--reset (-r)   : reset the database (drop the current schema)"
+--help (-h)
+--reset (-r)   : reset the database (drop the current schema)
+--initialize (-i) : include test data (to use with -r)"
 
 resetDatabase=false
+initDatabase=false
 while [[ $# > 0 ]]
 do
-	key="$1"
-	case $key in
-	    -h|--help)
-	    	echo $HELP
-	    	exit 0
-	    ;;
-	    -r|--reset)
-	    	resetDatabase=true
-	    ;;
-	    *)
-	        echo "Unknown parameter $1 exiting"
-	        exit 1
-	    ;;
-	esac
-	shift
+  key="$1"
+  case $key in
+    -h|--help)
+      echo $HELP
+      exit 0
+      ;;
+    -r|--reset)
+      resetDatabase=true
+      ;;
+    -i|--initialize)
+      initDatabase=true
+      ;;
+    *)
+      echo "Unknown parameter $1 exiting"
+      exit 1
+      ;;
+  esac
+  shift
 done
 
 if [ "$resetDatabase" = true ] ; then
-	echo ">> Reseting the database schema"
-mysql -h bizdockdb -u root --password=root <<EOF
+  echo ">> Reseting the database schema"
+  mysql -h bizdockdb -u root --password=root <<EOF
 DROP SCHEMA IF EXISTS maf;
 CREATE SCHEMA IF NOT EXISTS maf 
 DEFAULT CHARACTER SET utf8;
@@ -37,10 +42,10 @@ CREATE USER IF NOT EXISTS 'maf'@'%' IDENTIFIED BY 'maf';
 GRANT ALL ON maf.* TO 'maf'@'%';
 FLUSH PRIVILEGES;
 EOF
-	STATUS=$?
-	if [ $STATUS -ne 0 ]; then
-	  exit 1
-	fi
+  STATUS=$?
+  if [ $STATUS -ne 0 ]; then
+    exit 1
+  fi
 fi
 
 echo "---- BUILDING REPLACER ----"
@@ -62,12 +67,12 @@ mvn com.agifac.deploy:replacer-maven-plugin:replace -Dsource=dbmdl-framework-$ve
 unzip -d script merged-dbmdl-framework-$versionNumber.zip
 cd script/scripts
 chmod u+x *.sh
-  
+
 echo "---- BUILDING MAF DBMDL ----"
 mvn -f /opt/artifacts/maf-dbmdl/pom.xml clean install
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
-	exit 1
+  exit 1
 fi
 cd /opt/artifacts/maf-dbmdl/target
 versionNumber=$(ls maf-dbmdl-*-properties.zip | grep -oP '(?<=maf-dbmdl-).*(?=-properties.zip)')
@@ -89,10 +94,14 @@ if [ $STATUS -ne 0 ]; then
 fi
 
 if [ "$resetDatabase" = true ] ; then
-	echo ">> Initializing the database"
-	mysql -h bizdockdb -u root --password=root maf < /opt/artifacts/maf-desktop-app/conf/sql/init_base.sql
-	STATUS=$?
-	if [ $STATUS -ne 0 ]; then
-	  exit 1
-	fi
+  echo ">> Initializing the database"
+  mysql -h bizdockdb -u root --password=root maf < /opt/artifacts/maf-desktop-app/conf/sql/init_base.sql
+  STATUS=$?
+  if [ $STATUS -ne 0 ]; then
+    exit 1
+  fi
+  if [ "$initDatabase" ]; then
+    echo ">> Initializing test data"
+    mysql -h bizdockdb -u root --password=root maf < /opt/prepare/init_data.sql
+  fi
 fi
