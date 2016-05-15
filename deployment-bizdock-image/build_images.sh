f85cf802ac222c3f7af088092c604d6995bf8868
Pierre-Yves Cloux <me@pycloux.com>
2016-05-15 23:04:44 +0200

    Removing deployment code. Migrated to BizDock installation.

--- deployment-bizdock-image/build_images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "---- BUILDING BIZDOCK DATABASE IMAGE ----"
-docker build -f ./bizdockdb/Dockerfile -t taf/bizdock_mariadb:10.1.12 .
-
-echo "---- BUILDING BIZDOCK IMAGE ----"
-docker build -f ./bizdock/Dockerfile -t taf/bizdock:12.0.1 .
